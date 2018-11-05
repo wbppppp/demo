16ca97b2e1291cdfc725572ec0485f7a5121333b
wbp <1871960139@qq.com>
2018-11-05 11:31:16 +0800

    Test 第八次提交 Test2 3.0

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -3,8 +3,6 @@ package com.example.demo;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("第一次提交");
-        System.out.println("第五次提交");
-        System.out.println("第七次提交");
+        System.out.println("第八次提交");
     }
 }
--- src/main/java/com/example/demo/Test2.java
+++ src/main/java/com/example/demo/Test2.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("Test2 1.0");
-        System.out.println("Test2 2.0");
+        System.out.println("Test2 3.0");
     }
 }

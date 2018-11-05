425b43ea73c28df67ac50a3a7a8e5ef0e6e21fed
wbp <1871960139@qq.com>
2018-11-05 11:21:37 +0800

    Test 第六次提交 Test2 2.0

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("第五次提交");
+        System.out.println("第六次提交");
     }
 }
--- src/main/java/com/example/demo/Test2.java
+++ src/main/java/com/example/demo/Test2.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Test2 1.0");
+        System.out.println("Test2 2.0");
     }
 }

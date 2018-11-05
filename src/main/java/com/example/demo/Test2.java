1b023a2411a50a12d24ba7145faaefd241d46891
wbp <1871960139@qq.com>
2018-11-05 11:35:12 +0800

    Test 第九次提交 Test2 4.0

--- src/main/java/com/example/demo/Test2.java
+++ src/main/java/com/example/demo/Test2.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("Test2 3.0");
+        System.out.println("Test2 4.0");
     }
 }
